a8c6904706fe2e0f572d4e01db0854ba9c1a4dd2
Trevor Moon <trevor.r.moon@gmail.com>
2022-01-06 12:48:03 -0500

    Added python script (#11)
    * Need to test
    
    * feature: add python implementation
    
    * chore: remove commented code
    
    * refactor: add method to create env from string
    
    * test: add cli remove by name test
    
    * refactor: move program usage to cli function
    
    * refactor: update program options
    
    * refactor: update remove environment method
    
    update subprocess command to use fstring for easier changes
    
    * refactor: update all subprocess calls to use fstring
    
    * refactor: remove unused test
    
    * refactor: replace cli string method with constant
    
    * refactor: rename cli to main

---
+++ conda_remove_envs.py
@@ -0,0 +1,43 @@
+import subprocess as sp
+import argparse
+import re
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Remove conda environment(s)")
+    parser.add_argument(
+        "-n", "--name", metavar="ENVIRONMENT", nargs="+", help="name of environment(s)",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def list_envs():
+    """Returns conda environments as list"""
+    # conda env list output
+    cmd = f"conda env list".split()
+    proc = sp.run(cmd, stdout=sp.PIPE)
+    out = proc.stdout.decode("utf-8")
+
+    # return conda envs as list
+    env_list = re.findall("[\w-]+\s\s", out)
+    for i, env in enumerate(env_list):
+        env_list[i] = env.strip()
+    return env_list
+
+
+def remove_env(env):
+    """Remove conda environment by name"""
+    cmd = f"conda env remove -n {env}".split()
+    sp.run(cmd)
+
+
+def main():
+    """Run command-line program"""
+    args = parse_args()
+    for env in args.name:
+        remove_env(env)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_conda_remove_envs.py
@@ -0,0 +1,22 @@
+import pytest
+import subprocess
+
+import conda_remove_envs as cenv
+
+VALID_ENV_NAMES = [["test"], ["test", "test2"]]
+CLI_STR = "python conda_remove_envs.py"
+
+
+def create_env(env: str):
+    subprocess.run(f"conda create --name {env}".split())
+
+
+@pytest.mark.parametrize("envlist", VALID_ENV_NAMES)
+def test_removal_by_name_should_pass(envlist):
+    expected = cenv.list_envs()
+    for env in envlist:
+        create_env(env)
+    cmd = CLI_STR + " -n " + " ".join(envlist)
+    subprocess.run(cmd.split())
+    actual = cenv.list_envs()
+    assert expected == actual
