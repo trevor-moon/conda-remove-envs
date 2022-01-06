7e29b357ee91ab66682d915bb67570fd51b1a439
Trevor Moon <trevor.r.moon@gmail.com>
2022-01-06 11:25:58 -0500

    test: add cli remove by name test

--- test_conda_remove_envs.py
+++ test_conda_remove_envs.py
@@ -16,6 +16,20 @@ def create_conda_env(env_list):
         create_env(env)
 
 
+def cli_str() -> str:
+    return "python conda_remove_envs.py"
+
+
+@pytest.mark.parametrize("envlist", VALID_ENV_NAMES)
+def test_removal_by_name_should_pass(envlist):
+    expected = cenv.list_envs()
+    create_conda_env(envlist)
+    cmd = cli_str() + " -n " + " ".join(envlist)
+    subprocess.run(cmd.split())
+    actual = cenv.list_envs()
+    assert expected == actual
+
+
 @pytest.mark.parametrize("env_list", VALID_ENV_NAMES)
 def test_remove_env(env_list):
     exp_envs = cenv.list_envs()
