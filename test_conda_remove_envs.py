19c70f38c308ad6da8a48c35e4a0abb5feec2caa
Trevor Moon <trevor.r.moon@gmail.com>
2022-01-06 10:58:38 -0500

    refactor: add method to create env from string

--- test_conda_remove_envs.py
+++ test_conda_remove_envs.py
@@ -7,9 +7,13 @@ import conda_remove_envs as cenv
 VALID_ENV_NAMES = [["test"], ["test", "test2"]]
 
 
+def create_env(env: str):
+    subprocess.run(f"conda create --name {env}".split())
+
+
 def create_conda_env(env_list):
     for env in env_list:
-        subprocess.run(["conda", "create", "--name", env])
+        create_env(env)
 
 
 @pytest.mark.parametrize("env_list", VALID_ENV_NAMES)
