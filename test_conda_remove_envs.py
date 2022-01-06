7f6e754b147de1066e28cbdc55dd349c010cfb3f
Trevor Moon <trevor.r.moon@gmail.com>
2022-01-06 12:23:54 -0500

    refactor: remove unused test

--- test_conda_remove_envs.py
+++ test_conda_remove_envs.py
@@ -11,11 +11,6 @@ def create_env(env: str):
     subprocess.run(f"conda create --name {env}".split())
 
 
-def create_conda_env(env_list):
-    for env in env_list:
-        create_env(env)
-
-
 def cli_str() -> str:
     return "python conda_remove_envs.py"
 
@@ -23,17 +18,9 @@ def cli_str() -> str:
 @pytest.mark.parametrize("envlist", VALID_ENV_NAMES)
 def test_removal_by_name_should_pass(envlist):
     expected = cenv.list_envs()
-    create_conda_env(envlist)
+    for env in envlist:
+        create_env(env)
     cmd = cli_str() + " -n " + " ".join(envlist)
     subprocess.run(cmd.split())
     actual = cenv.list_envs()
     assert expected == actual
-
-
-@pytest.mark.parametrize("env_list", VALID_ENV_NAMES)
-def test_remove_env(env_list):
-    exp_envs = cenv.list_envs()
-    create_conda_env(env_list)
-    cenv.remove(env_list)
-    act_envs = cenv.list_envs()
-    assert set(exp_envs) == set(act_envs)
